d7097957f5934ea4361f583de30a6623dd48143c
bryancurtis <curtispro97@gmail.com>
2021-09-28 14:24:47 -0700

    Added a lot of wet button code: color changing, value display, and on click functionality

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,7 @@ console.log("Sanity ✅");
 $("#save-name").on("click", function(event) {
     $('#name-modal').hide();
     getName();
+    game.startTimer();
 });
 
 const getName = function getName() {
@@ -25,14 +26,84 @@ const game = {
     evolutions: 0,
     progress: 0,
     name: '',
+    hunger: 0,
+    sleep: 0,
+    bored: 0,
 
     // Game Methods
     startTimer() {
         setInterval(function(){
-            game.time += 1;
+            game.time++;
             game.progress += 2;
-            console.log(game.time);
+            
+            // button code
+            game.hunger++;
+            game.sleep++;
+            game.bored++;
+            $("#hunger").text(`Hunger ${game.hunger}`);
+            $("#sleep").text(`Sleepiness ${game.sleep}`);
+            $("#bored").text(`Boredom ${game.bored}`);
+            game.buttonColorChange();
+
+            // console.log(game.time);
             $("#dynamic").attr("aria-valuenow", game.progress).css("width", game.progress  + "%");
         }, 1000);
+    },
+
+    // Function to change button color based on value
+    buttonColorChange() {
+        // Hunger Button
+        if (game.hunger < 3) {
+            $("#hunger").removeClass("btn-warning btn-danger").addClass("btn-success");
+        } else if (game.hunger >= 3 && game.hunger <= 6) {
+            $("#hunger").removeClass("btn-danger btn-success").addClass("btn-warning");
+        } else if (game.hunger > 6) {
+            $("#hunger").removeClass("btn-success btn-warning").addClass("btn-danger");
+        };
+
+        // Sleep Button
+        if (game.sleep < 3) {
+            $("#sleep").removeClass("btn-warning btn-danger").addClass("btn-success");
+        } else if (game.sleep >= 3 && game.sleep <= 6) {
+            $("#sleep").removeClass("btn-danger btn-success").addClass("btn-warning");
+        } else if (game.sleep > 6) {
+            $("#sleep").removeClass("btn-success btn-warning").addClass("btn-danger");
+        };
+
+        // Bored Button
+        if (game.bored < 3) {
+            $("#bored").removeClass("btn-warning btn-danger").addClass("btn-success");
+        } else if (game.bored >= 3 && game.bored <= 6) {
+            $("#bored").removeClass("btn-danger btn-success").addClass("btn-warning");
+        } else if (game.bored > 6) {
+            $("#bored").removeClass("btn-success btn-warning").addClass("btn-danger");
+        };
+    },
+
+    // Trying to organize and condense button code into functions
+    buttonClick() {
+
+    },
+
+    // Maybe a one stop function for keeping values from between 0-10
+    rangeCheck() {
+
     }
 }
+
+// Button onClick value reducer funtions
+$("#hunger").on("click", function(event) {
+    $("#hunger").text(`Hunger ${game.hunger}`);
+    game.hunger--;
+    game.buttonColorChange();
+});
+$("#sleep").on("click", function(event) {
+    $("#sleep").text(`Sleepiness ${game.sleep}`);
+    game.sleep--;
+    game.buttonColorChange();
+});
+$("#bored").on("click", function(event) {
+    $("#bored").text(`Boredom ${game.bored}`);
+    game.bored--;
+    game.buttonColorChange();
+});

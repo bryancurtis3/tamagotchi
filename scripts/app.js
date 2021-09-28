c898f14f7745f1d3cec77b5ca66bd76bc3e3910c
bryancurtis <curtispro97@gmail.com>
2021-09-28 12:17:49 -0700

    Added a timer method, currently includes progress bar funcitonality

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,24 @@
 console.log("Sanity ✅");
+
+// === INTERVAL / TIMER ===
+
+// let gameTime = 0;
+// setInterval(function(){
+//     game.time += 1;
+// }, 1000);
+
+const game = {
+    time: 0,
+    evolutions: 0,
+    progress: 0,
+
+    // Game Methods
+    startTimer() {
+        setInterval(function(){
+            game.time += 1;
+            game.progress += 2;
+            console.log(game.time);
+            $("#dynamic").attr("aria-valuenow", game.progress).css("width", game.progress  + "%");
+        }, 1000);
+    }
+}

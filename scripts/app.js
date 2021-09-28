fa84cac24f92bc55ed3694ce40f49193c9f879ff
bryancurtis <curtispro97@gmail.com>
2021-09-28 16:18:34 -0700

    Added rangeCheck and progressCheck methods which govern evolution stage and loss conditions

--- scripts/app.js
+++ scripts/app.js
@@ -25,25 +25,45 @@ const game = {
     time: 0,
     evolutions: 0,
     progress: 0,
+    stage: 1,
     name: '',
+    // values: {
+    //     hunger: 0,
+    //     sleep: 0,
+    //     bored: 0,
+    // },
+    clicks: {
+        hunger: 0,
+        sleep: 0,
+        bored: 0,
+    },
     hunger: 0,
     sleep: 0,
     bored: 0,
 
     // Game Methods
     startTimer() {
-        setInterval(function(){
+        const interval = setInterval(function(){
             game.time++;
-            game.progress += 2;
+            game.progress += 5;
             
             // button code
             game.hunger++;
             game.sleep++;
             game.bored++;
+            // game.values.hunger++;
+            // game.values.sleep++;
+            // game.values.bored++;
             $("#hunger").text(`Hunger ${game.hunger}`);
             $("#sleep").text(`Sleepiness ${game.sleep}`);
             $("#bored").text(`Boredom ${game.bored}`);
             game.buttonColorChange();
+            game.progressCheck();
+            // if (game.rangeCheck() === true) {
+            //     alert("You Lose!");
+            //     clearInterval(interval);
+            // }
+            
 
             // console.log(game.time);
             $("#dynamic").attr("aria-valuenow", game.progress).css("width", game.progress  + "%");
@@ -52,6 +72,7 @@ const game = {
 
     // Function to change button color based on value
     buttonColorChange() {
+
         // Hunger Button
         if (game.hunger < 3) {
             $("#hunger").removeClass("btn-warning btn-danger").addClass("btn-success");
@@ -87,23 +108,63 @@ const game = {
 
     // Maybe a one stop function for keeping values from between 0-10
     rangeCheck() {
+        // Checks if above 0 and below 11
+        // Hunger
+        if (game.hunger < 0) {
+            game.hunger = 0;
+        };
+        // Sleep
+        if (game.sleep < 0) {
+            game.sleep = 0;
+        };
+        // Bored
+        if (game.bored < 0) {
+            game.bored = 0;
+        };
+        if (game.hunger >= 11 || game.sleep >= 11 ||game.bored >= 11) return true;
+    },
+
+    progressCheck() {
+        
+        if (game.progress === 100 && game.stage === 1) {
+            console.warn("100 reached")
+            game.progress = 0;
+            $("#character-sprite").attr("src", "assets/amoeba.png");
+            console.log($("#character-sprite").attr("src"))
+            game.stage++;
+        } else if (game.progress === 100 && game.stage === 2) {
+            console.warn("200 reached")
+            game.progress = 0;
+            $("#character-sprite").attr("src", "assets/fish.png");
+            console.log($("#character-sprite").attr("src"));
+            game.stage++;
+        };
+
 
     }
-}
+}; // ===== END OF GAME OBJECT =====
+
+console.log(Object.keys(game.values)[0])
 
 // Button onClick value reducer funtions
 $("#hunger").on("click", function(event) {
     $("#hunger").text(`Hunger ${game.hunger}`);
     game.hunger--;
     game.buttonColorChange();
+    game.rangeCheck();
+    game.clicks.hunger++;
 });
 $("#sleep").on("click", function(event) {
     $("#sleep").text(`Sleepiness ${game.sleep}`);
     game.sleep--;
     game.buttonColorChange();
+    game.rangeCheck();
+    game.clicks.sleep++;
 });
 $("#bored").on("click", function(event) {
     $("#bored").text(`Boredom ${game.bored}`);
     game.bored--;
     game.buttonColorChange();
+    game.rangeCheck();
+    game.clicks.bored++;
 });
---
+++ scripts/graveyard.js
@@ -0,0 +1,20 @@
+        
+        
+// taken from 
+
+        // DRYING ATTEMPT -- failed
+        // for (let i = 0; i < Object.values(game.values).length; i++) {
+        //     let value = Object.values(game.values)[i];
+        //     const key = Object.keys(game.values)[i];
+        //     console.log(value)
+        //     value = parseInt(value);
+        //     console.log(`$${key}`);
+            
+        //     if (value < 3) {
+        //         $(`$${key}`).removeClass("btn-warning btn-danger").addClass("btn-success");
+        //     } else if (value >= 3 && value <= 6) {
+        //         $(`$${key}`).removeClass("btn-danger btn-success").addClass("btn-warning");
+        //     } else if (value > 6) {
+        //         $(`$${key}`).removeClass("btn-success btn-warning").addClass("btn-danger");
+        //     };
+        // }
